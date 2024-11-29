e25725661208830e5424fea4e6c51798ff001a41
maayan <mayanpotu500@gmail.com>
2024-11-29 15:20:54 +0200

    small fixes

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    lazy = true,
-}
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    Lazy = true,
-    ft = { "markdown", "norg", "rmd", "org" },
-    config = function()
-        vim.keymap.set("n", "<leader>md", "<cmd>lua require('render-markdown').toggle()<CR>")
-    end,
-}
