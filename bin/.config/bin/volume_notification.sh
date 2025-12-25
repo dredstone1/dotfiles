647913a3013c89fa3946f2bd1608839e4336e8ae
maayan <mayanpotu500@gmail.com>
2025-12-25 19:35:23 +0200

    improved code

---
+++ bin/.config/bin/volume_notification.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+APP_NAME="media"
+NOTIFY_ID=9992  # Different ID from the progress script
+
+get_volume() {
+    vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '/Volume:/ {print $2}')
+    [ -z "$vol" ] && echo 0 && return
+    percent=$(awk "BEGIN {printf \"%d\", $vol * 100}")
+    [ "$percent" -gt 100 ] && percent=100
+    [ "$percent" -lt 0 ] && percent=0
+    echo "$percent"
+}
+
+get_mute_status() {
+    mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+
+    if [ "$mute" = "yes" ]; then
+        echo "Muted"
+    else
+        echo "Unmuted"
+    fi
+}
+
+show_notification() {
+    volume=$(get_volume)
+    mute_status=$(get_mute_status)
+
+    dunstify \
+        -r "$NOTIFY_ID" \
+        -h "int:value:$volume" \
+        "Volume: $volume% $mute_status"
+}
+
+change_volume() {
+    wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ "$1"
+}
+
+toggle_mute_volume() {
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+}
+
+case "$1" in
+    scroll)
+        change_volume "$2"
+        show_notification
+        ;;
+    muteVolume)
+        toggle_mute_volume
+        show_notification
+        ;;
+    popup)
+        show_notification
+        ;;
+    *)
+        echo "Usage: $0 {scroll <volume>|muteVolume|popup}"
+        exit 1
+        ;;
+esac
+
