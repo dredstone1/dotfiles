f000f1526ff52f5da67f9332293c55a57add58cf
maayan <mayanpotu500@gmail.com>
2026-07-18 10:07:15 +0300

    adding super-tab

--- bin/.config/bin/rgb_controller.sh
+++ bin/.config/bin/rgb_controller.sh
@@ -1,2 +1,2 @@
-openrgb --device 1 --mode static --color FFFFFF
-openrgb --device "Logitech G Pro RGB Mechanical Gaming Keyboard" --mode Static --color FB91F6
+# openrgb --device 1 --mode static --color FFFFFF
+openrgb --device "Logitech G Pro RGB Mechanical Gaming Keyboard" --mode Static --color CC86FB
